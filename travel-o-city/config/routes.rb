264a153f14d79e5a4e065b20bbdbdbb57a314622
ksarangmath <kartiksarangmath@gmail.com>
2017-01-08 18:13:39 -0600

    Add files via upload

--- travel-o-city/config/routes.rb
+++ travel-o-city/config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+ root to: 'application#home'
+
+
+  # CREATE
+ get '/new_user' => 'users#new'
+ get '/create_user' => 'users#create'
+
+ # READ
+ get '/users' => 'users#index'
+ get '/users/:id' => 'users#show'
+
+ # UPDATE
+ get '/users/:id/edit' => 'users#edit'
+ get '/users/:id/update' => 'users#update'
+
+ # DELETE
+ get '/users/:id/destroy' => 'users#destroy'
+ #------------------------------
+
+
+
+
 end
